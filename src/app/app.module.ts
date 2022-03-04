d3810e82d7bcffa3765c737980b0b5467c48fe70
khairellah <zakariakhairellah@gmail.com>
2022-03-04 16:27:40 +0100

    Zozor Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BackgroundComponent } from './background/background.component';
+import { ArticleComponent } from './article/article.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BackgroundComponent,
+    ArticleComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
